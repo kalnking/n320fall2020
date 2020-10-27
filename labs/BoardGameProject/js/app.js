c4dd8fb8f081145b2a68f9c34be2358ca809deef
KaleneKingery <kalnking@iu.edu>
2020-10-27 19:59:24 -0400

    new update

--- labs/BoardGameProject/js/app.js
+++ labs/BoardGameProject/js/app.js
@@ -1,10 +1,18 @@
 let st1 = document.getElementsByClassName("st1");
-console.log(st1);
+//console.log(st1);
 
-let colorChange = st1.getAttribute("fill");
-console.log(colorChange);
+//let colorChange = st1.getAttribute("fill");
+//console.log(colorChange);
 
-st1.addEventListener("click", trailCLick);
-function trailCLick(event) {
-  event.target.style.fill = "#ff0000";
+function colorChange(e) {
+  st1.style.fill = "#ff0000";
 }
+
+for (var i = 0; i < st1.length; i++) {
+  st1[i].addEventListener("click", colorChange);
+}
+//st1.addEventListener("click", trailCLick);
+//console.log("working");
+//function trailCLick(event) {
+// event.target.style.fill = "#ff0000";
+//}
